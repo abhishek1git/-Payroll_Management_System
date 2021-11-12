7350050b06d06626046ef3487fde562a3873b427
Abhishek Kumar <86146227+abhishek1git@users.noreply.github.com>
2021-11-12 14:50:01 +0530

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/styleMenu.css">
+    <title>Menu</title>
+<body>
+    <div id="menucont">
+        <h1>Menu</h1>
+        <div id="menubox">
+            <a href="add_employee/AddEmployee.html"><h3>Add Employee</h3></a><br>
+            <a href="search/search.html"><h3>Search Employee</h3></a><br>
+            <a href="calculate/calculate.html"><h3>Calculate PayRoll </h3></a><br>
+            <button type="button" style="width:60%;";align:center;>Exit</button>
+        </div>
+    </div>
+    
+</body>
+</html
